c0003d3f43c1461bee25eca82de06137f186fd65
Tibor Djurica Potpara <tibor.djurica@ojdip.net>
2020-09-26 20:57:00 +0100

    fix

--- server/constants.js
+++ server/constants.js
@@ -119,7 +119,7 @@ exports.actionSchema = Joi.alternatives()
 exports.scorePresets = scorePresets;
 exports.defaultSettings = settingsSchema.validate({}).value;
 exports.shutdownTimeout = 5000;
-exports.heartbeatInterval = Number(process.env.PP_HEARTBEAT_INTERVAL) || 5000
-exports.heartbeatTimeout = Number(process.env.PP_HEARTBEAT_TIMEOUT) || 10000
+exports.heartbeatInterval = Number(process.env.PP_HEARTBEAT_INTERVAL) || 5000;
+exports.heartbeatTimeout = Number(process.env.PP_HEARTBEAT_TIMEOUT) || 10000;
 // For how long to persist the session data after the last client disconnected.
 exports.sessionTtl = Number(process.env.PP_SESSION_TTL) || 60000;
